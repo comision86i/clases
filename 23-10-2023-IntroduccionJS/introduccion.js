ca23e54dc6d787ea81e9bbb4d6ae852b02bb350b
Comision86i <comision86i.rollingcode@gmail.com>
2023-10-23 11:30:21 -0300

    Se crean archivos de introducción a JS

---
+++ 23-10-2023-IntroduccionJS/introduccion.js
@@ -0,0 +1,11 @@
+let nombre = 'Julio';
+let edad = 43;
+const altura = 1.74;
+let casado = true;
+
+console.log(nombre, edad, altura, casado);
+nombre = 'Ignacio';
+console.log(nombre);
+console.log(edad);
+console.log(altura);
+console.log(casado);
